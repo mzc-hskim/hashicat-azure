ad0583ae1e01b31e839178b1b467e79c88a8cfe2
hs.kim <hs.kim@mz.co.kr>
2023-12-20 14:35:08 +0000

    Added network module

--- main.tf
+++ main.tf
@@ -200,3 +200,10 @@ resource "null_resource" "configure-cat-app" {
     }
   }
 }
+
+module "network" {
+  source  = "app.terraform.io/hyunsik-org/network/azurerm"
+  version = "3.5.0"
+  # insert required variables here
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
+}
